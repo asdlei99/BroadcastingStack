c4990117d4b69b40059e69f57d704c5de608dfa5
shanhui <shanhui@agora.io>
2018-08-08 15:36:47 +0800

    add setup file for dependencies

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    install_requires=[
+        "sphinx-rtd-theme",
+        "recommonmark",
+        "unicode_ids",
+        "sphinx-markdown-tables",
+    ],
+)
