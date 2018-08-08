9130246f4f4881b355f9f178984c17347d7b5511
shanhui <shanhui@agora.io>
2018-08-08 16:21:00 +0800

    change the dependency tree

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     install_requires=[
-        "Markdown",
-        "sphinx-rtd-theme",
-        "recommonmark",
         "unicode_ids",
         "sphinx-markdown-tables",
+        "sphinx-rtd-theme",
+        "recommonmark",
+        "Markdown",
     ],
 )
